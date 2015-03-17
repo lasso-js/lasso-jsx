113be21b133472fce4323e264015e817a3970e74
Phillip Gates-Idem <phillip.idem@gmail.com>
2015-03-17 10:17:28 -0400

    Fixes #1

--- optimizer-jsx-plugin.js
+++ optimizer-jsx-plugin.js
@@ -1,54 +1,67 @@
 var fs = require('fs');
-var react = require('react-tools');
-var reactDomPragma = require('react-dom-pragma');
+var reactTools = require('react-tools');
 
+try {
+    var reactVersion = require('react/package.json').version;
+    var versionParts = reactVersion.split('.');
+    var reactMajor = Number(versionParts[0]);
+    var reactMinor = Number(versionParts[1]);
+
+    var reactToolsVersion = require('react-tools/package.json').version;
+    var versionParts = reactToolsVersion.split('.');
+    var reactToolsMajor = Number(versionParts[0]);
+    var reactToolsMinor = Number(versionParts[1]);
+
+    if ((reactToolsMajor !== reactMajor) || (reactToolsMinor !== reactMinor)) {
+        var logger = require('raptor-logging').logger(module);
+        logger.warn('optimizer-jsx is using react-tools ' + reactToolsVersion + ' to compile JSX but react ' + reactVersion + ' is installed.\n' +
+            'This version mismatch may cause incompatibilities. Consider installing react-tools in your project and ' +
+            'then uninstall and reinstall optimizer-jsx so that your version of react-tools will be used.\n' +
+            'If you find any problems please open an issue at https://github.com/raptorjs/optimizer-jsx/issues');
+    }
+} catch(e) {
+    // React is not a required module to do the build so ignore this error
+}
+
+function _compileFile(path, callback) {
+	fs.readFile(path, {
+		encoding: 'utf8'
+	}, function(err, src) {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, reactTools.transform(src, {}));
+	});
+}
 module.exports = function(pageOptimizer, config) {
-    pageOptimizer.dependencies.registerJavaScriptType(
-        'jsx',
-        {
-            properties: {
-                'path': 'string'
-            },
-            init: function(optimizerContext, callback) {
-                if (!this.path) {
-                    return callback(new Error('"path" is required'));
-                }
-                this.path = this.resolvePath(this.path);
-                callback();
-            },
-            read: function(context, callback) {
-                var path = this.path,
-                    transformedCode;
+	pageOptimizer.dependencies.registerJavaScriptType(
+		'jsx', {
+			properties: {
+				'path': 'string'
+			},
+			init: function(optimizerContext, callback) {
+				if (!this.path) {
+					return callback(new Error('"path" is required'));
+				}
+				this.path = this.resolvePath(this.path);
+				callback();
+			},
+			read: function(context, callback) {
+                _compileFile(this.path, callback);
+			},
+			getSourceFile: function() {
+				return this.path;
+			}
+		});
 
-                fs.readFile(path, {encoding: 'utf8'}, function(err, src) {
-                    if (err) {
-                        return callback(err);
-                    }
-                    transformedCode = react.transform(reactDomPragma(src), {});
-                    callback(null, transformedCode);
-                });
-            },
-            getSourceFile: function() {
-                return this.path;
-            }
-        });
+	pageOptimizer.dependencies.registerRequireExtension(
+		'jsx', {
+			read: function(path, optimizerContext, callback) {
+				_compileFile(path, callback);
+			},
 
-    pageOptimizer.dependencies.registerRequireExtension(
-        'jsx', {
-            read: function(path, optimizerContext, callback) {
-                var transformedCode;
-
-                fs.readFile(path, {encoding: 'utf8'}, function(err, src) {
-                    if (err) {
-                        return callback(err);
-                    }
-                    transformedCode = react.transform(reactDomPragma(src), {});
-                    callback(null, transformedCode);
-                });
-            },
-
-            getLastModified: function(path, optimizerContext, callback) {
-                optimizerContext.getFileLastModified(path, callback);
-            }
-        });
+			getLastModified: function(path, optimizerContext, callback) {
+				optimizerContext.getFileLastModified(path, callback);
+			}
+		});
 };
--- test/test.js
+++ test/test.js
@@ -9,41 +9,44 @@ var fs = require('fs');
 var jsxPlugin = require('../'); // Load this module just to make sure it works
 var optimizer = require('optimizer');
 
-describe('optimizer-jsx' , function() {
+describe('optimizer-jsx', function() {
 
 
-    it('should compile a simple jsx file', function(done) {
-        var pageOptimizer = optimizer.create({
-                fileWriter: {
-                    fingerprintsEnabled: false,
-                    outputDir: nodePath.join(__dirname, 'static')
-                },
-                bundlingEnabled: true,
-                plugins: [
-                    {
-                        plugin: jsxPlugin,
-                        config: {
+	it('should compile a simple jsx file', function(done) {
+		var pageOptimizer = optimizer.create({
+			fileWriter: {
+				fingerprintsEnabled: false,
+				outputDir: nodePath.join(__dirname, 'static')
+			},
+			bundlingEnabled: true,
+			plugins: [
+				{
+					plugin: jsxPlugin,
+					config: {
 
-                        }
+					}
                     }
                 ]
-            });
+		});
 
-        pageOptimizer.optimizePage({
-                name: 'testPage',
-                dependencies: [
+		pageOptimizer.optimizePage({
+				name: 'testPage',
+				dependencies: [
                     nodePath.join(__dirname, 'fixtures/simple.jsx')
                 ]
-            },
-            function(err, optimizedPage) {
-                if (err) {
-                    return done(err);
-                }
+			},
+			function(err, optimizedPage) {
+				if (err) {
+					return done(err);
+				}
 
-                var output = fs.readFileSync(nodePath.join(__dirname, 'static/testPage.js'), 'utf8');
-                expect(output).to.equal('/** @jsx React.DOM */\nReact.renderComponent(React.DOM.h1(null, "simple"), document.body);');
-                done();
-            });
-    });
+				var output = fs.readFileSync(nodePath.join(__dirname,
+					'static/testPage.js'), 'utf8');
+				expect(output).to.equal(
+					'React.renderComponent(React.createElement(\"h1\", null, \"simple\"), document.body);'
+				);
+				done();
+			});
+	});
 
 });
